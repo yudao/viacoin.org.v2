61090b312c1d003a6bd315100a95e955275f6b7f
Yudao <plugman@free.fr>
2018-05-07 16:51:40 +0200

    #6 - Created softer animation for features

--- src/components/feature/card.js
+++ src/components/feature/card.js
@@ -5,47 +5,19 @@ import './styles.sass'
 /** @jsx preact.h */
 
 export default class FeatureCard extends preact.Component {
-  constructor() {
-    super();
-    this.toggleActiveClass = this.toggleActiveClass.bind(this);
-    this.enableActiveClass = this.enableActiveClass.bind(this);
-    this.disableActiveClass = this.disableActiveClass.bind(this);
-    this.activeClassName = "is-active";
-    this.state = {
-      activeClass: ""
-    }
-  }
-
-  enableActiveClass(e) {
-    this.setState({ activeClass: this.activeClassName });
-  }
-
-  disableActiveClass(e) {
-    this.setState({ activeClass: "" });
-  }
-
-  toggleActiveClass(e) {
-    const currentActiveClass = (this.state.activeClass == "") ? this.activeClassName : "";
-    e.target.closest(".columns").querySelectorAll("." + this.activeClassName).forEach(c => {
-      c.classList.remove(this.activeClassName);
-    });
-    this.setState({ activeClass: currentActiveClass });
-  }
-
   render(props, state) {
     const {title, text } = props;
     const icon = this.props.children[0];
-    // const isActive = this.state.activeClass || false;
-    const classes = "card is-vcentered has-text-centered " + this.state.activeClass;
-    return <article class={ classes } onclick={ this.toggleActiveClass } onmouseover={ this.enableActiveClass } onmouseout={ this.disableActiveClass }>
+    const classes = "card is-vcentered has-text-centered";
+    return <article class={ classes }>
       <div class="card-image">
         <span class="is-icon">{ icon }</span>
         <h4 class="title is-4">{ title }</h4>
       </div>
-      <div class={"card-content " + this.state.activeClass}>
+      <div class="card-content">
         <div class="content">{ text }</div>
       </div>
-      <footer class={"card-footer " + this.state.activeClass}>
+      <footer class="card-footer">
         <p class="card-footer-item">...</p>
       </footer>
     </article>
