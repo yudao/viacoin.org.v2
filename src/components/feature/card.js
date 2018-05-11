70f720f5f732127bf4a48ddcbd184f3b3c43f978
Yudao <plugman@free.fr>
2018-05-11 05:44:26 +0200

    #6 - Fixed features doesn't above at first click

--- src/components/feature/card.js
+++ src/components/feature/card.js
@@ -36,7 +36,7 @@ export default class FeatureCard extends preact.Component {
     const {title, text } = props;
     const icon = this.props.children[0];
     const classes = "card is-vcentered has-text-centered " + state.activeClass;
-    return <article class={ classes } onclick={ this.toggleActiveClass } onmouseover={ this.enableActiveClass } onmouseout={ this.disableActiveClass }>
+    return <article class={ classes } ontouchstart={ this.toggleActiveClass } onmouseover={ this.enableActiveClass } onmouseout={ this.disableActiveClass }>
       <div class="card-image">
         <span class="is-icon">{ icon }</span>
         <h4 class="title is-4">{ title }</h4>
